994e193184d203c081e76234c9bc076d0629b898
umgaolu <koybethere2015@163.com>
2026-01-13 21:39:31 +0800

    fix Next compliation issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,6 @@
 const nextConfig = {
-  reactStrictMode: true,
-  transpilePackages: [
-    "antd",
-    "@ant-design/icons",
-    "rc-util",
-    "rc-pagination",
-    "rc-picker",
-    "rc-tree",
-    "rc-table",
-    "rc-notification",
-    "rc-tooltip"
-  ]
+  transpilePackages: ["antd", "@ant-design/icons", "@ant-design/icons-svg"]
 };
 
 export default nextConfig;
+
