83b41f8927b3ded4e65def926ed56993a0882bf7
umgaolu <koybethere2015@163.com>
2026-01-13 22:02:02 +0800

    fix: transpile rc-tree to avoid ESM resolution error

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 const nextConfig = {
-  transpilePackages: ["antd", "@ant-design/icons", "@ant-design/icons-svg"]
+  transpilePackages: [
+    "antd",
+    "@ant-design/icons",
+    "@ant-design/icons-svg",
+    "rc-util",
+    "rc-picker",
+    "rc-table",
+    "rc-tree"
+  ]
 };
 
 export default nextConfig;
-
